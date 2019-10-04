1809373784b8d647b6284bb2339de092d71083c1
Aleemendoza <alecolomendoza@gmail.com>
2019-10-04 03:23:56 -0300

    hice algunos cambios con respecto a como se ve y como funciona la pagina

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var imagenPerfil = document.getElementById("imagenPerfil");
 var circulo3 = document.getElementById("Circulo3");
 var lenguaje = document.getElementById("lenguajes");
 var titulos = document.getElementsByClassName("titulos");
-
+var contenidosCirculos = document.getElementById("contenidosCirculo");
+var identificador =  document.getElementById("identificador")
+var contenido = document.getElementById("contenido")
 imagenPerfil.addEventListener("click",agregarClases);
 function agregarClases(){
 
     circulo.classList.add("active");
     circulo3.classList.add("rotar2");
+    imagenPerfil.classList.add("desaparecer");
+    identificador.classList.add("ocultar")
+    contenido.classList.add("aparecer")
 
 }
-lenguaje.addEventListener("click",mostrarTitulo);
-    function mostrarTitulo(){
 
-        parrafoLenguaje.classList.add("visibilidad")
-        
-    }
